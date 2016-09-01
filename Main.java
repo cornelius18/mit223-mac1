7dd5e8740c92ccbbfe6aee4a9a48fb5a0b586076
cornelius18 <emmanuel18.41@gmail.com>
2016-09-01 17:15:49 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package machine1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+public static void main(String [] args) throws IOException{
+Path fileDir = Paths.get(folderPath);
+Path fileDir = Paths.get(folderPath);
+}
+}
